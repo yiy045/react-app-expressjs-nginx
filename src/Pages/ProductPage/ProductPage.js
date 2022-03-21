73d847aef685c91d50e6b577e818b23bab47fba6
miketitan58 <michs800@yahoo.com>
2022-03-21 12:25:37 -0500

    Added Basic Product Page

---
+++ src/Pages/ProductPage/ProductPage.js
@@ -0,0 +1,14 @@
+import "./ProductPage.css"
+
+function Product(){
+    return(
+            <div>
+                <div className = "App">
+                    <header><b>PRODUCT NAME HERE</b></header>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+    );
+}
+
+export default Product;
