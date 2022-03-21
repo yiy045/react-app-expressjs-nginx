2a783bf9f3258301480ee8e1c7e7681faa926d3d
miketitan58 <michs800@yahoo.com>
2022-03-21 14:38:39 -0500

    updated product page

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -3,9 +3,21 @@ import "./ProductPage.css"
 function Product(){
     return(
             <div>
-                <div className = "App">
-                    <header><b>PRODUCT NAME HERE</b></header>
-                    <button>Add to Cart</button>
+                <div className = "App1">
+                    <div className = "header1">
+                        <b>WorldWide Frames</b> - <i>See A Better World</i>
+                    </div>
+                    {/* create an overall panel, with header 2, the image, and the button inside of it */}
+                    <div className = "panel1">
+                        <div className = "header2">    
+                            <header2>
+                                <p>Name: </p>
+                                <p>Manufacturer: </p>
+                                <p> Cost: </p>
+                            </header2>
+                        </div>
+                        <button>Add to Cart</button>
+                    </div>
                 </div>
             </div>
     );
