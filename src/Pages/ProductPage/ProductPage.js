d9c00c7aacae27673f3c141c62a65dc2537311e4
miketitan58 <michs800@yahoo.com>
2022-03-22 11:40:48 -0500

    working on printing variables
    doesnt work but the framework is there

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -1,8 +1,19 @@
 import "./ProductPage.css"
 
 function Product(){
+
+    /* temo variables */
+    let test_name = "glasses01";
+    let test_manufacturer = "Stark Industries";
+    let test_cost = 99.99;
+
     return(
             <div>
+                <script>
+                    let name = test_name;
+                    let manufacturer = test_manufacturer;
+                    let cost = test_cost;
+                </script>
                 <div className = "App1">
                     <div className = "header1">
                         <b>WorldWide Frames</b> - <i>See A Better World</i>
@@ -10,9 +21,9 @@ function Product(){
                     {/* create an overall panel, with header 2, the image, and the button inside of it */}
                     <div className = "panel1">
                         <div className = "header2">    
-                                <p>Name: </p>
-                                <p>Manufacturer: </p>
-                                <p>Cost: </p>
+                                <p>Name: <var>name</var></p>
+                                <p>Manufacturer: <var>manufacturer</var></p>
+                                <p>Cost: <var>cost</var> </p>
                         </div>
                         <button>Add to Cart</button>
                     </div>
