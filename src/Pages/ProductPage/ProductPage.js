9c5f02600890836cd02c6095a5e4e6b022041969
miketitan58 <michs800@yahoo.com>
2022-03-23 11:33:37 -0500

    Revert "working on printing variables"
    This reverts commit d9c00c7aacae27673f3c141c62a65dc2537311e4.

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -1,19 +1,8 @@
 import "./ProductPage.css"
 
 function Product(){
-
-    /* temo variables */
-    let test_name = "glasses01";
-    let test_manufacturer = "Stark Industries";
-    let test_cost = 99.99;
-
     return(
             <div>
-                <script>
-                    let name = test_name;
-                    let manufacturer = test_manufacturer;
-                    let cost = test_cost;
-                </script>
                 <div className = "App1">
                     <div className = "header1">
                         <b>WorldWide Frames</b> - <i>See A Better World</i>
@@ -21,9 +10,9 @@ function Product(){
                     {/* create an overall panel, with header 2, the image, and the button inside of it */}
                     <div className = "panel1">
                         <div className = "header2">    
-                                <p>Name: <var>name</var></p>
-                                <p>Manufacturer: <var>manufacturer</var></p>
-                                <p>Cost: <var>cost</var> </p>
+                                <p>Name: </p>
+                                <p>Manufacturer: </p>
+                                <p>Cost: </p>
                         </div>
                         <button>Add to Cart</button>
                     </div>
