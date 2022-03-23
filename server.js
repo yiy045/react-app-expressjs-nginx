02827849cb41c741f338201c9fd5a379cd258b14
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2022-03-23 08:31:20 -0500

    Hash password on registration

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express(); // create express app
 const http = require('http');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 app.use(cors());
 
 const bodyParser = require('body-parser');
@@ -34,25 +36,42 @@ db.connect((err) => {
     console.log("Connected to MySQL server.");
 });
 
-app.post("/create", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const email = req.body.email;
-  const address = req.body.address;
-  const firstname = req.body.firstname;
-  const lastname = req.body.lastname;
-  const phone = req.body.phone;
+app.post("/register", (req, res) => {
 
-  const query = "INSERT INTO account (username, email, address, password, first_name, last_name, phone_num) VALUES (?, ?, ?, ?, ?, ?, ?);";
-  db.query(
-    query,
-    [username, email, address, password, firstname, lastname, phone],
-    );
-  return res.json(200);
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const address = req.body.address;
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const phone = req.body.phone;
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
+        }
+
+        const data = [
+            username,
+            email,
+            address,
+            hash,
+            firstname,
+            lastname,
+            phone
+        ];
+
+        const query = "INSERT INTO account (username, email, address, password, first_name, last_name, phone_num) VALUES (?, ?, ?, ?, ?, ?, ?);";
+        db.query(
+            query, data,
+            (err, result) => {
+                if (err) {
+                    res.send({ err: err });
+                }
+            }
+        );
+    });
 });
-
-app
-
 // start express server on port 5000
 server.listen(5000, () => {
     console.log('NodeJS server running');
