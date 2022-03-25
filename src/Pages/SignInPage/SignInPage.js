1df73fbb9415b48231712a55a995647229aea108
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2022-03-25 11:36:27 -0500

    SIgnIn page formatting and CSS height

--- src/Pages/SignInPage/SignInPage.js
+++ src/Pages/SignInPage/SignInPage.js
@@ -2,56 +2,51 @@ import React, { useState } from 'react';
 import Axios from "axios";
 import './SignInPage.css';
 
-function SignIn()
-{
+function SignIn() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const [loginStatus, setLoginStatus] = useState("");
 
-    const login = () =>
-    {
-        Axios.post("http://localhost:5000/signin", 
-        {
-            username: username,
-            password: password,
-        }).then((response) =>
-        {
-            if(response.data.message)
+    const login = () => {
+        Axios.post("http://localhost:5000/signin",
             {
-                setLoginStatus(response.data.message);
-            } else
-            {
-                setLoginStatus(response.data[0].username);
-            }
-            
-        });
+                username: username,
+                password: password,
+            }).then((response) => {
+                if (response.data.message) {
+                    setLoginStatus(response.data.message);
+                } else {
+                    setLoginStatus(response.data[0].username);
+                }
+
+            });
     };
 
     return (
-        <div className="signin-wrapper">
-            <h1>Sign In</h1>
-                    <input 
-                        type="text"
-                        placeholder="Username..."
-                        onChange={(e) => 
-                        {
-                            setUsername(e.target.value);
-                        }} 
-                    />
-                
-                    <input 
-                        type="password"
-                        placeholder="Password..."
-                        onChange={(e) => 
-                        {
-                            setPassword(e.target.value);
-                        }}
-                    />
+        <div className="signin-webpage">
+            <div className="signin-wrapper">
+                <h1>Sign In</h1>
+                <input
+                    type="text"
+                    placeholder="Username..."
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password..."
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
                 <div>
                     <button onClick={login}> Login </button>
                 </div>
-            <h1>{loginStatus}</h1>
+                <h1>{loginStatus}</h1>
+            </div>
         </div>
     );
 }
