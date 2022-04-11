a219b22f2b085eae2a6a7782cd88666ce399332d
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2022-04-11 13:07:31 -0500

    CSS conflict fix

--- src/Pages/AccountPage/AccountPage.js
+++ src/Pages/AccountPage/AccountPage.js
@@ -3,46 +3,45 @@ import "./AccountPage.css"
 import Axios from "axios";
 import { useHistory } from "react-router-dom";
 
-
-
 function getAccountInformation() {
-
   return (
     <body>
-      <div class="container">
-        <div class="title">Profile</div>
-        <div class="content">
-          <form action="#">
-            <div class="user-details">
-              <div class="input-box">
-                <span class="details"><strong>First Name</strong></span>
-                <input type="text" required></input>
+      <div className="AccountPage">
+        <div className="container">
+          <div className="title">Profile</div>
+          <div className="content">
+            <form action="#">
+              <div className="user-details">
+                <div className="input-box">
+                  <span class="details"><strong>First Name</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div className="input-box">
+                  <span class="details"><strong>Last Name</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div className="input-box">
+                  <span class="details"><strong>Username</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div className="input-box">
+                  <span class="details"><strong>Email</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div className="input-box">
+                  <span class="details"> <strong>Phone Number</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div className="input-box">
+                  <span class="details"><strong>Address</strong></span>
+                  <input type="text" required></input>
+                </div>
               </div>
-              <div class="input-box">
-                <span class="details"><strong>Last Name</strong></span>
-                <input type="text" required></input>
+              <div className="button">
+                <input type="submit" value="Profile"></input>
               </div>
-              <div class="input-box">
-                <span class="details"><strong>Username</strong></span>
-                <input type="text" required></input>
-              </div>
-              <div class="input-box">
-                <span class="details"><strong>Email</strong></span>
-                <input type="text" required></input>
-              </div>
-              <div class="input-box">
-                <span class="details"> <strong>Phone Number</strong></span>
-                <input type="text" required></input>
-              </div>
-              <div class="input-box">
-                <span class="details"><strong>Address</strong></span>
-                <input type="text" required></input>
-              </div>
-            </div>
-            <div class="button">
-              <input type="submit" value="Profile"></input>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </body>
