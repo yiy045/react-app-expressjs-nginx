9c14084ffe74a90a4760fc80773c0e0571a9ebff
kamryn-fey <kamrynfey12@gmail.com>
2022-04-15 12:08:58 -0500

    formatting product page

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -7,22 +7,19 @@ function Product() {
     let cost = 99.99;
     return (
         <div className="product-background">
+            <div className="ProductPage">
             {/* create an overall panel, with header 2, the image, and the button inside of it */}
-                <div className="container">
-                    <div className="image1">
-                        <img src={image}
-                            alt=""
-                            width="400"
-                            height="400" />
-                    </div>
+                <div className="container1">
+                        <img src={image}/>
                 </div>
-                <div className="container">
-                    <p><b>Name: </b> {name}</p>
+                <div className="container2">
+                    <p><b>Names: </b> {name}</p>
                     <p><b>Manufacturer: </b>{manufacturer} </p>
                     <p><b>Cost: </b>{cost}</p>
                     <button>Add to Cart</button>
                 </div>
             </div>
+        </div>
     );
 }
 
