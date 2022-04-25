c0e762c0f56514803b48c5cab504928522582af4
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2022-04-25 11:34:26 -0500

    Template for updating user info

--- src/Pages/AccountPage/AccountPage.js
+++ src/Pages/AccountPage/AccountPage.js
@@ -21,57 +21,80 @@ function Account() {
 
   useEffect(() => {
     Axios.get("http://localhost:5000/signin").then((response) => {
-        if (!(response.data.user)) {
-            history.push("/");
-        }
-        else{
-          setFirstName(response.data.user.first_name);
-          setLastName(response.data.user.last_name);
-          setUsername(response.data.user.username);
-          setEmail(response.data.user.email);
-          setPhoneNum(response.data.user.phone_num);
-          setAddress(response.data.user.address);
-        }
+      if (!(response.data.user)) {
+        history.push("/");
+      }
+      else {
+        setFirstName(response.data.user.first_name);
+        setLastName(response.data.user.last_name);
+        setUsername(response.data.user.username);
+        setEmail(response.data.user.email);
+        setPhoneNum(response.data.user.phone_num);
+        setAddress(response.data.user.address);
+      }
     })
   }, [])
 
+
+  const Update = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:5000/update-user").then((response) => {
+      
+    })
+  }
+
+  console.log(firstName);
+
   return (
-      <div className="AccountPage">
-        <div className="container">
-          <div className="title">Profile</div>
-          <div className="content">
-            <form action="#">
-              <div className="user-details">
-                <div className="input-box">
-                  <span className="details"><strong>First Name</strong></span>
-                  <input type="text" value={firstName} disabled></input>
-                </div>
-                <div className="input-box">
-                  <span className="details"><strong>Last Name</strong></span>
-                  <input type="text" value={lastName} disabled ></input>
-                </div>
-                <div className="input-box">
-                  <span className="details"><strong>Username</strong></span>
-                  <input type="text" value={username} disabled ></input>
-                </div>
-                <div className="input-box">
-                  <span className="details"><strong>Email</strong></span>
-                  <input type="text" value={email} disabled ></input>
-                </div>
-                <div className="input-box">
-                  <span className="details"> <strong>Phone Number</strong></span>
-                  <input type="text" value={phoneNum} disabled ></input>
-                </div>
-                <div className="input-box">
-                  <span className="details"><strong>Address</strong></span>
-                  <input type="text" value={address} disabled ></input>
-                </div>
+    <div className="AccountPage">
+      <div className="container">
+        <div className="title">Profile</div>
+        <div className="content">
+          <form action="#">
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details"><strong>First Name</strong></span>
+                <input type="text" value={firstName} onChange={(event) => {
+                  setFirstName(event.target.value)
+                }}></input>
               </div>
-            </form>
-          </div>
-          <Link to="/order-history">View Order History</Link>
+              <div className="input-box">
+                <span className="details"><strong>Last Name</strong></span>
+                <input type="text" value={lastName} onChange={(event) => {
+                  setLastName(event.target.value)
+                }}></input>
+              </div>
+              <div className="input-box">
+                <span className="details"><strong>Username</strong></span>
+                <input type="text" value={username} disabled ></input>
+              </div>
+              <div className="input-box">
+                <span className="details"><strong>Email</strong></span>
+                <input type="text" value={email} disabled ></input>
+              </div>
+              <div className="input-box">
+                <span className="details"> <strong>Phone Number</strong></span>
+                <input type="text" value={phoneNum} onChange={(event) => {
+                  setPhoneNum(event.target.value)
+                }}></input>
+              </div>
+              <div className="input-box">
+                <span className="details"><strong>Address</strong></span>
+                <input type="text" value={address} onChange={(event) => {
+                  setAddress(event.target.value);
+                }}></input>
+              </div>
+            </div>
+            <div className="Update">
+              <button onClick={Update}>
+                Update
+              </button>
+            </div>
+          </form>
         </div>
+        <Link to="/order-history">View Order History</Link>
       </div>
+    </div>
 
   );
 }
