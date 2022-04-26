df6693228f916448176cd079fd78396e5659a941
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2022-04-26 00:01:30 -0500

    Better images (I hope)

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,27 +2,30 @@ import "./HomePage.css"
 import Deals from "./DealsOfTheDay"
 import React from "react";
 
-
 import 'font-awesome/css/font-awesome.min.css';
 
 function Home(props) {
 
   if (props.location.alert) {
     alert(props.location.alert)
-    props.location.alert="";
+    props.location.alert = "";
   }
 
   return (
     <div>
       <div className="HomePage">
+        
         <div className="HomeHeader">
-          <header><b>WorldWide Frames</b> - <i>See A Better World</i></header>
+        <div className="head">
+          <b>WorldWide Frames</b> - <i>See A Better World</i>
         </div>
-        <h2><i>Items on Sale</i></h2>
+          <header></header>
+
+        </div>
+        <h2>Deals of the Day</h2>
         <div className="Frames">
           <div className="DoD">
-            <h3>Deals of the Day</h3>
-            <Deals/>
+            <Deals />
           </div>
         </div>
       </div>
